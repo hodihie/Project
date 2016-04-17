ca9f822989d835cab9cb6b02fd0772574bb3baad
hodihie <hodihie@gmail.com>
2016-04-17 12:21:28 +0700

    add ApointmentConstants  class

---
+++ HTDatlichkham/src/haui/library/ApointmentConstants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package haui.library;
+
+/**
+ * @author Dinh Hieu
+ *
+ */
+public class ApointmentConstants {
+
+	// apointment length
+	public static final int APOINTMENT_LENGTH;
+
+	static {
+		APOINTMENT_LENGTH = 15;
+
+	}
+
+}
