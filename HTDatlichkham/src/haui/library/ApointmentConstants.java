9f75a288f127c6ae51240407af11e3c62ec3512b
hodihie <hodihie@gmail.com>
2016-04-19 10:36:31 +0700

    add constant START_HOUR, START_MINUTE, END_HOUR, END_MINUTE

--- HTDatlichkham/src/haui/library/ApointmentConstants.java
+++ HTDatlichkham/src/haui/library/ApointmentConstants.java
@@ -12,9 +12,29 @@ public class ApointmentConstants {
 	// apointment length
 	public static final int APOINTMENT_LENGTH;
 
+	// start hour 08
+	public static final int START_HOUR;
+
+	// start minute 00
+	public static final int START_MINUTE;
+
+	// end hour 17
+	public static final int END_HOUR;
+
+	// end minute 30
+	public static final int END_MINUTE;
+
 	static {
+
 		APOINTMENT_LENGTH = 15;
 
+		START_HOUR = 8;
+
+		START_MINUTE = 0;
+
+		END_HOUR = 17;
+
+		END_MINUTE = 30;
 	}
 
 }
